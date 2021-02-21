b1df0482215100ce0eb0161de263d19f0ea1e87d
ryuuji3 Josh Lalonde <mori.ryuuji3@gmail.com>
2021-02-20 23:32:09 -0500

    game of life iterations

--- src/cell.rs
+++ src/cell.rs
@@ -1,3 +1,5 @@
+#[repr(u8)]
+#[derive(Clone, Copy)]
 pub enum Cell {
     Dead = 0,
     Alive = 1,
--- src/grid.rs
+++ src/grid.rs
@@ -11,18 +11,7 @@ pub struct Grid {
     cells: Vec<Cell>,
 }
 
-#[wasm_bindgen]
 impl Grid {
-    pub fn new(width: u32, height: u32) -> Grid {
-        let cells = Grid::generate_cells(width, height);
-
-        Grid {
-            width,
-            height,
-            cells,
-        }
-    }
-
     fn generate_cells(width: u32, height: u32) -> Vec<Cell> {
         let grid_size: usize = (width * height) as usize;
         let mut cells = Vec::<Cell>::with_capacity(grid_size);
@@ -38,7 +27,62 @@ impl Grid {
         cells
     }
 
+    fn count_live_neighbors(&self, index: usize) -> u8 {
+        let rows = self.height as usize;
+
+        self.cells[index - 1] as u8  // left
+            + self.cells[index + 1] as u8 // right
+            + self.cells[index - rows] as u8 // top
+            + self.cells[index + rows] as u8 // bottom
+            + self.cells[index - rows - 1] as u8 // top left
+            + self.cells[index - rows + 1] as u8 // top right
+            + self.cells[index + rows - 1] as u8 // bottom left
+            + self.cells[index + rows + 1] as u8 // bottom right
+    }
+}
+
+// Exported to JS
+#[wasm_bindgen]
+impl Grid {
+    pub fn new(width: u32, height: u32) -> Grid {
+        let cells = Grid::generate_cells(width, height);
+
+        Grid {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let mut next_generation = self.cells.clone();
+
+        for (index, cell) in self.cells.iter().enumerate() {
+            next_generation[index] = match (cell, self.count_live_neighbors(index)) {
+                // Live cell with less than 2 neighbors dies
+                (Cell::Alive, neighbors) if neighbors < 2 => Cell::Dead,
+                // Live cell with 2 OR 3 neighbors lives
+                (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                // Live cell with more than 3 neighbors dies
+                (Cell::Alive, neighbors) if neighbors > 3 => Cell::Dead,
+                // Dead cell with 3 neighbors comes to life
+                (Cell::Dead, 3) => Cell::Alive,
+                (cell, _) => *cell,
+            }
+        }
+
+        self.cells = next_generation
+    }
+
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
     }
 }
